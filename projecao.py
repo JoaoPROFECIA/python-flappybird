84b3669389731293458266c357d15c4f0ecc00e8
Joao Victor <victorjv3288@gmail.com>
2022-06-10 16:32:04 -0300

    Update projecao.py

--- projecao.py
+++ projecao.py
@@ -1,14 +1,22 @@
 from tokenize import group
-import pygame
+import pygame, random
 from pygame.locals import *
 
-from flappy import GAME_SPEED
+from flappy import GAME_SPEED, GROUND_WIDTH, PIPE_WIDTH
 
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 800
 SPEED = 10
 GRAVITY = 1
 GAME_SPEED = 10
+
+GROUND_WIDTH = 2 * SCREEN_WIDTH
+GROUND_HEIGHT = 100
+
+PIPE_WIDTH = 80
+PIPE_HEIGHT = 500
+
+PIPE_GAP = 200
 
 class Bird(pygame.sprite.Sprite):
     def __init__(self):
@@ -23,6 +31,8 @@ class Bird(pygame.sprite.Sprite):
         self.current_image = 0
 
         self.image = pygame.image.load('bluebird-upflap.png').convert_alpha()
+        
+        self.mask = pygame.mask.from_surface(self.image)
 
         self.rect = self.image.get_rect()
         self.rect[0] = SCREEN_WIDTH / 2.5
@@ -40,17 +50,53 @@ class Bird(pygame.sprite.Sprite):
     def bump(self):
         self.speed = -SPEED
 
-class Ground(pygame.sprite.Sprite):
-    def __init__(self, width, height):
+
+class Pipe(pygame.sprite.Sprite):
+
+    def __init__(self, inverted, xpos, ysize):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('base.png')
-        self.image = pygame.transform.scale(self.image, (width, height))
+
+        self.image = pygame.image.load('pipe-red.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (PIPE_HEIGHT, PIPE_WIDTH))
 
         self.rect = self.image.get_rect()
+        self.rect[0] = xpos
 
-    def update():
+        if inverted:
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect[1] = - (self.rect[3] - ysize)
+        else:
+            self.rect[1] = SCREEN_HEIGHT - ysize
+
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def update(self):
         self.rect[0] -= GAME_SPEED
 
+
+class Ground(pygame.sprite.Sprite):
+    def __init__(self, xpos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('base.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (GROUND_WIDTH, GROUND_WIDTH))
+
+        self.mask = pygame.mask.from_surface(self.image)
+
+        self.rect = self.image.get_rect()
+        self.rect[0] = xpos
+        self.rect[1] = SCREEN_WIDTH - GROUND_WIDTH
+
+    def update(self):
+        self.rect[0] -= GAME_SPEED
+
+def is_off_screen(sprite):
+    return sprite.rect[0] < -sprite.rect[2]
+
+def get_random_pipe_height(xpos):
+    size = random.randint(100, 300)
+    pipe = Pipe(False, xpos, size)
+    pipe_inverted = Pipe(True, xpos, SCREEN_HEIGHT - size - PIPE_GAP)
+    return pipe, pipe_inverted
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -63,8 +109,15 @@ bird = Bird()
 bird_group.add(bird)
 
 ground_group = pygame.sprite.Group()
-ground = Ground(SCREEN_WIDTH, 100)
-ground_group.add(ground)
+for i in range(2):
+    ground = Ground(GROUND_WIDTH * i)
+    ground_group.add(ground)
+
+pipe_group = pygame.sprite.Group()
+for i in range(2):
+    pipes= get_random_pipes(SCREEN_WIDTH * i + 800)
+    pipe_group.add(pipes[0])
+    pipe_group.add(pipes[1])
 
 clock = pygame.time.Clock()
 
@@ -80,10 +133,31 @@ while True:
     
     screen.blit(BACKGROUND, (0, 0))
 
+    if is_off_screen(ground_group.sprites()[0]):
+        ground_group.remove(ground_group.sprites()[0])
+
+        new_ground = Ground(GROUND_WIDTH -20)
+        ground_group.add(new_ground)
+
+    if is_off_screen(pipe_group.sprites()[0]):
+        pipe_group.remove(pipe_group.sprites()[0])
+        pipe_group.remove(pipe_group.sprites()[0])
+
+        pipes = get_random_pipes(SCREEN_WIDTH * 2)
+
+        pipe_group.add(pipes[0])
+        pipe_group.add(pipes[1])
+
     bird_group.update()
     ground_group.update()
+    pipe_group.update()
 
     bird_group.draw(screen)
+    pipe_group.draw(screen)
     ground_group.draw(screen)
-
+    
     pygame.display.update()
+
+    if (pygame.sprite.groupcollide(bird_group, ground_group, False, False, pygame.sprite.collide_mask)) or (pygame.sprite.groupcollide(bird_group, pipe_group, False, False, pygame.sprite.collide_mask)):
+        input()
+        break
